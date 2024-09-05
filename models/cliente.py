3ab15a8b9e4493052f36de7d2b0d593bdb236b65
Robelio Barreto <robeliobasantos@gmail.com>
2024-09-05 12:38:05 -0300

    Criando estrutura de pastas e arquivos

---
+++ models/cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+    
---
+++ models/pessoa_fisica.py
@@ -0,0 +1,9 @@
+from models.cliente  import Cliente
+
+class PessoaFisica(Cliente):
+    
+    def __init__ (self, nome, data_nascimento, cpf, endereco):
+        super().__init__ (endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento 
+        self.cpf = cpf
